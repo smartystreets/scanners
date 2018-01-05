6164345dd775384ddf03759a78bf8b3580e74f4a
Michael Whatcott <mdwhatcott@gmail.com>
2018-01-05 11:44:04 -0700

    Included clear examples in the same way that encoding/csv does.

---
+++ csv/example_test.go
@@ -0,0 +1,65 @@
+package csv_test
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/smartystreets/scanners/csv"
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+func ExampleScanner() {
+	in := strings.Join([]string{
+		`first_name,last_name,username`,
+		`"Rob","Pike",rob`,
+		`Ken,Thompson,ken`,
+		`"Robert","Griesemer","gri"`,
+	}, "\n")
+	scanner := csv.NewScanner(strings.NewReader(in))
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Record())
+	}
+
+	if err := scanner.Error(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Output:
+	// [first_name last_name username]
+	// [Rob Pike rob]
+	// [Ken Thompson ken]
+	// [Robert Griesemer gri]
+}
+
+// This example shows how csv.Scanner can be configured to handle other
+// types of CSV files.
+func ExampleScannerOptions() {
+	in := strings.Join([]string{
+		`first_name;last_name;username`,
+		`"Rob";"Pike";rob`,
+		`# lines beginning with a # character are ignored`,
+		`Ken;Thompson;ken`,
+		`"Robert";"Griesemer";"gri"`,
+	}, "\n")
+
+	scanner := csv.NewScanner(strings.NewReader(in), csv.Comma(';'), csv.Comment('#'))
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Record())
+	}
+
+	if err := scanner.Error(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Output:
+	// [first_name last_name username]
+	// [Rob Pike rob]
+	// [Ken Thompson ken]
+	// [Robert Griesemer gri]
+}
