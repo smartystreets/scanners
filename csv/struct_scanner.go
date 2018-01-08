cc0263d1694e45f53a1571c3c23973271d9afa10
Michael Whatcott <mdwhatcott@gmail.com>
2018-01-08 15:42:43 -0700

    StructScanner unmarshals an entire record into a struct with 'csv' tags.

---
+++ csv/struct_scanner.go
@@ -0,0 +1,54 @@
+package csv
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+)
+
+type StructScanner struct {
+	*ColumnScanner
+}
+
+func NewStructScanner(reader io.Reader, options ...Option) (*StructScanner, error) {
+	inner, err := NewColumnScanner(reader, options...)
+	if err != nil {
+		return nil, err
+	}
+	return &StructScanner{ColumnScanner: inner}, nil
+}
+
+func (this *StructScanner) Populate(v interface{}) error {
+	type_ := reflect.TypeOf(v)
+	if type_.Kind() != reflect.Ptr {
+		return fmt.Errorf("Provided value must be reflect.Ptr. You provided [%v] ([%v]).", v, type_.Kind())
+	}
+
+	value := reflect.ValueOf(v)
+	if value.IsNil() {
+		return fmt.Errorf("The provided value was nil. Please provide a non-nil pointer.")
+	}
+
+	this.populate(type_.Elem(), value.Elem())
+	return nil
+}
+
+func (this *StructScanner) populate(type_ reflect.Type, value reflect.Value) {
+	for x := 0; x < type_.NumField(); x++ {
+		column := type_.Field(x).Tag.Get("csv")
+
+		_, found := this.columnIndex[column]
+		if !found {
+			continue
+		}
+
+		field := value.Field(x)
+		if field.Kind() != reflect.String {
+			continue
+		} else if !field.CanSet() {
+			continue
+		}
+
+		field.SetString(this.Column(column))
+	}
+}
---
+++ csv/struct_scanner_test.go
@@ -0,0 +1,49 @@
+package csv
+
+import (
+	"testing"
+
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
+)
+
+func TestStructScannerFixture(t *testing.T) {
+	gunit.Run(new(StructScannerFixture), t)
+}
+
+type StructScannerFixture struct {
+	*gunit.Fixture
+	scanner *StructScanner
+	err     error
+	users   []TaggedUser
+}
+
+func (this *StructScannerFixture) Setup() {
+	this.scanner, this.err = NewStructScanner(reader(csvCanon))
+	this.So(this.err, should.BeNil)
+}
+
+func (this *StructScannerFixture) ScanAll() {
+	for this.scanner.Scan() {
+		var user TaggedUser
+		this.scanner.Populate(&user)
+		this.users = append(this.users, user)
+	}
+}
+
+func (this *StructScannerFixture) Test() {
+	this.ScanAll()
+
+	this.So(this.scanner.Error(), should.BeNil)
+	this.So(this.users, should.Resemble, []TaggedUser{
+		{FirstName: "Rob", LastName: "Pike", Username: "rob"},
+		{FirstName: "Ken", LastName: "Thompson", Username: "ken"},
+		{FirstName: "Robert", LastName: "Griesemer", Username: "gri"},
+	})
+}
+
+type TaggedUser struct {
+	FirstName string `csv:"first_name"`
+	LastName  string `csv:"last_name"`
+	Username  string `csv:"username"`
+}
